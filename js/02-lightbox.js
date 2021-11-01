c4d21de10783343b07af8cf6205172c2f4ed838b
forysiak-vova <volodja.forus@gmail.com>
2021-11-01 15:58:51 +0100

    додав галерею і розмітку в завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click',ongalleryContainerClick);
+
+
+function createImgCardsMarkup(galleryItems) {
+   return galleryItems.map(({preview, original}) => {
+   
+      return  `
+  <div class="gallery__item">
+  
+
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="Image description" />
+</a>
+</div>
+   `
+   }).join('');
+   
+}
 
 console.log(galleryItems);
+
+function ongalleryContainerClick(e) {
+   const isGalleryClass = e.target.classList.contains('.gallery__image');
+   if (!isGalleryClass) {
+      return;
+   }
+   console.log(e.target);
+   
+};
