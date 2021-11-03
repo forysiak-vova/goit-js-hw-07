81ce28bb4cb7ca09c582490349cb45c3ea4143fc
forysiak-vova <volodja.forus@gmail.com>
2021-11-03 19:00:23 +0100

    поправки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,7 @@
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
